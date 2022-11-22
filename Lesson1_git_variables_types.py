e1a7426188f8d5b7d4b758a42b1f0cbf7c0dd688
Sigizmund <40463616+samnesvoy@users.noreply.github.com>
2022-11-22 22:24:16 +0200

    escape-последовательности ДЗ

--- Lesson1_git_variables_types.py
+++ Lesson1_git_variables_types.py
@@ -11,3 +11,12 @@ i = int()  # целые числа
 f = float()  # дробные числа
 s = str()  # строки
 b = bool()  # логические данные (истина/ложь(True/False))
+
+some_string = '"Какая-то строка"'
+print(some_string,end='')
+some_string2 = "'Какая-то строка2'"
+print(some_string2)
+some_string3 = '"Какая-то\' строка3"'
+print(some_string3)
+some_string4 = '"Какая\b-то \nстро\tка4"'
+print(some_string4,variable)
