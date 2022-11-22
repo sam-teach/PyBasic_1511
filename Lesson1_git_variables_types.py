28a5d026438869a67e2368b8638b9cdaefdfded9
Sigizmund <40463616+samnesvoy@users.noreply.github.com>
2022-11-22 21:50:16 +0200

    Переменные типы

--- Lesson1_git_variables_types.py
+++ Lesson1_git_variables_types.py
@@ -7,3 +7,7 @@ apples_in_box = 89  # snake_case
 applesInBox = 89  # camelCase
 # Ctrl+Alt+L <- автоматическое форматирование кода по pep8
 
+i = int()  # целые числа
+f = float()  # дробные числа
+s = str()  # строки
+b = bool()  # логические данные (истина/ложь(True/False))
