d355daf18f8d95162d02fee7c3e5d99e86c11330
Sigizmund <40463616+samnesvoy@users.noreply.github.com>
2022-11-29 21:23:16 +0200

    операторы

--- Lesson2_types_f-strings_in-out.py
+++ Lesson2_types_f-strings_in-out.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 i = int()
 f = float()
 s = str()
@@ -27,7 +29,60 @@ print(result, type(result))
 print()
 # чуть-чуть строк
 print('Hello')
-print('Hello'+'world')
-print('Hello'*6)
-print(False*'world')
-
+print('Hello' + 'world')
+print('Hello' * 6)
+print(False * 'world')
+# операторы
+'''
+арифметические
++ - сложение
+- - вычитание
+* - произведение
+/ - деление
+** - возведение в степень
+// - деление нацело(без остатка) только int
+% - получение остатка(без целой части) только int
+= - присвоение ("копирование" справа налево)
+'''
+a = 3
+b = 5
+a = b
+print(a, b)
+print(17 // 3)
+print(17 % 3)
+print(sqrt(17))
+print()
+'''
+логические
+> - левое больше правого
+< - левое меньше правого
+>= - левое больше либо равно правого
+<= - левое меньше либо равно правого
+== - левое равно правому
+or - вернет истину если левое ИЛИ правое истина
+and - вернет истину если левое И правое истина
+^(xor) - вернет истину если ЛИБО левое ЛИБО правое истина
+not  - вернет истину если элемент ложь 
+in - вернет истину если элемент слева принадлежит элементу справа
+'''
+print('or')
+print(True or False)
+print(2 == 2.0 or 4 > 8)
+print(2 > 2.0 or 4 > 8)
+print('and')
+print(True and False)
+print(2 == 2.0 and 4 > 8)
+print(2 > 2.0 and 4 > 8)
+print(2 == 2.0 and 4 < 8)
+print('xor')
+print(True ^ False)
+print((2 == 2.0) ^ (4 > 8))
+print((2 > 2.0) ^ (4 > 8))
+print((2 == 2.0) ^ (4 < 8))
+print('not')
+print(not True)
+print(not 2 == 2.0)
+print(not 4 > 8)
+print('in')
+print('a' in 'Anton')
+print('a' in 'Mila')
