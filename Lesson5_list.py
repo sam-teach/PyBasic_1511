0ba00930da5f8428f6c60afef3c8eb8c808c9dc3
Sigizmund <40463616+samnesvoy@users.noreply.github.com>
2022-12-09 20:37:06 +0200

    создание списков

--- Lesson5_list.py
+++ Lesson5_list.py
@@ -1,10 +1,22 @@
-# проверка, является ли число простым
-number = int(input('Input number: '))
-for i in range(2, number // 2 + 1):
-    if number % i == 0:
-        print('Число не простое')
-        break
-else:
-    # этот блок выполнится только если цикл завершился сам,
-    # без прерывания(break)
-    print('число простое')
+# # проверка, является ли число простым
+# number = int(input('Input number: '))
+# for i in range(2, number // 2 + 1):
+#     if number % i == 0:
+#         print('Число не простое')
+#         break
+# else:
+#     # этот блок выполнится только если цикл завершился сам,
+#     # без прерывания(break)
+#     print('число простое')
+
+# создание списка
+my_list = list()
+my_list2 = []
+my_list3 = [1, 2, 3, 'VASA', 5.2, my_list]
+my_list4 = list('qwerty')
+print('my_list', my_list)
+print('my_list2', my_list2)
+print('my_list3', my_list3)
+print('my_list4', my_list4)
+my_list5 = list(my_list3) + my_list4
+print('my_list5',my_list5)
