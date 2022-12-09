df7806217a7b302e00717eb858e0c237b83c32a3
Sigizmund <40463616+samnesvoy@users.noreply.github.com>
2022-12-09 20:20:48 +0200

    решение ДЗ(простое число)

--- Lesson1_git_variables_types.py
+++ Lesson1_git_variables_types.py
@@ -1,22 +1,24 @@
-variable = 7
-print(variable)
-variable = 'VAsa'
+# variable = 7
+# print(variable)
+# variable = 'VAsa'
+#
+# print(variable)
+# apples_in_box = 89  # snake_case
+# applesInBox = 89  # camelCase
+# # Ctrl+Alt+L <- автоматическое форматирование кода по pep8
+#
+# i = int()  # целые числа
+# f = float()  # дробные числа
+# s = str()  # строки
+# b = bool()  # логические данные (истина/ложь(True/False))
+#
+# some_string = '"Какая-то строка"'
+# print(some_string,end='')
+# some_string2 = "'Какая-то строка2'"
+# print(some_string2)
+# some_string3 = '"Какая-то\' строка3"'
+# print(some_string3)
+# some_string4 = '"Какая\b-то \nстро\tка4"'
+# print(some_string4,variable)
 
-print(variable)
-apples_in_box = 89  # snake_case
-applesInBox = 89  # camelCase
-# Ctrl+Alt+L <- автоматическое форматирование кода по pep8
 
-i = int()  # целые числа
-f = float()  # дробные числа
-s = str()  # строки
-b = bool()  # логические данные (истина/ложь(True/False))
-
-some_string = '"Какая-то строка"'
-print(some_string,end='')
-some_string2 = "'Какая-то строка2'"
-print(some_string2)
-some_string3 = '"Какая-то\' строка3"'
-print(some_string3)
-some_string4 = '"Какая\b-то \nстро\tка4"'
-print(some_string4,variable)
