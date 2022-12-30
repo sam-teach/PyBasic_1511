369fce7cd5a8d9537fed2f65d4faf8b25bd4ebc9
Sigizmund <40463616+samnesvoy@users.noreply.github.com>
2022-12-30 21:22:28 +0200

    Dictionary пример roll dice

--- Lesson9_dictionary.py
+++ Lesson9_dictionary.py
@@ -37,44 +37,73 @@
 # }
 # print(study_class['Petrov']['name'])
 
-'''
-product = {
-name : ''
-description : ''
-price : 0
+# '''
+# product = {
+# name : ''
+# description : ''
+# price : 0
+# }
+# '''
+# products = {
+#     1: {
+#         'name': 'meat',
+#         'description': 'Crocodile meat',
+#         'price': 412
+#     },
+#     2: {
+#         'name': 'juice',
+#         'description': 'Crocodile juice',
+#         'price': 612
+#     },
+#     3: {
+#         'name': 'leg',
+#         'description': 'Crocodile leg',
+#         'price': 112.8
+#     },
+#     4: {
+#         'name': 'egg',
+#         'description': 'Chicken egg',
+#         'price': 30.5
+#     },
+# }
+# for id in products:
+#     print(products[id]['name'], end=' ')
+# print()
+# cart = []
+# cart.append(products[2])
+# cart.append(products[4])
+# print('Your choice is ', end='')
+# for product in cart:
+#     print(product['name'], end=' ')
+# print()
+# print('Your price is ', end='')
+# print(sum(product['price'] for product in cart))
+from random import randint
+from time import sleep
+
+cube_dict = {
+    1: 'this is 1',
+    2: 'this is 2',
+    3: 'this is 3',
+    4: 'this is 4',
+    5: 'this is 5',
+    6: 'this is 6',
 }
-'''
-products = {
-    1: {
-        'name': 'meat',
-        'description': 'Crocodile meat',
-        'price': 412
-    },
-    2: {
-        'name': 'juice',
-        'description': 'Crocodile juice',
-        'price': 612
-    },
-    3: {
-        'name': 'leg',
-        'description': 'Crocodile leg',
-        'price': 112.8
-    },
-    4: {
-        'name': 'egg',
-        'description': 'Chicken egg',
-        'price': 30.5
-    },
-}
-for id in products:
-    print(products[id]['name'], end=' ')
-print()
-cart = []
-cart.append(products[2])
-cart.append(products[4])
-print('Your choice is ', end='')
-for product in cart:
-    print(product['name'], end=' ')
-print()
-print('Your price is ', end='')
-print(sum(product['price'] for product in cart))
+while True:
+    choice = input('Playing game? Y/N')
+    if choice == 'Y':
+        comp = randint(1, 7)
+        sleep(2)
+        print(f'Computer turn: {cube_dict[comp]}')
+        user = 0
+        roll = input('You rolling, print "roll"')
+        if roll == 'roll':
+            user = randint(1, 7)
+            sleep(2)
+            print(f'Your turn is {cube_dict[user]}')
+        if user > comp:
+            print('You win')
+        else:
+            print('You lose')
+    else:
+        break
