12d2a17f5239374cd750ebe70dbad05e1ce51529
Sigizmund <40463616+samnesvoy@users.noreply.github.com>
2023-01-10 22:06:17 +0200

    Serialize CSV

--- Lesson12_serialize.py
+++ Lesson12_serialize.py
@@ -1,9 +1,9 @@
 '''
 Сериализация
 '''
-# JSON
-import json
-
+# # JSON
+# import json
+#
 user = {
     'firstname': 'Taras',
     'lastname': 'Bulba',
@@ -16,25 +16,46 @@ user = {
         'sombrero',
     ],
 }
-json_string = json.dumps(user)
-print('json_string ->', type(json_string), json_string)
-new_object = json.loads(json_string)
-print('new_object ->', type(new_object), new_object)
+# json_string = json.dumps(user)
+# print('json_string ->', type(json_string), json_string)
+# new_object = json.loads(json_string)
+# print('new_object ->', type(new_object), new_object)
+#
+# li = [
+#     'sword',
+#     'horse',
+#     'sigar',
+#     'sombrero',
+# ]
+# json_li_string = json.dumps(li)
+# print('json_li_string ->', type(json_li_string), json_li_string)
+# new_li_object = json.loads(json_li_string)
+# print('new_li_object ->', type(new_li_object), new_li_object)
+#
+# with open('files/serialize.txt', 'wt') as file:
+#     json.dump(user, file)
+#
+# with open('files/serialize.txt', 'rt') as file:
+#     new_object = json.load(file)
+#     print('new_object ->', type(new_object), new_object)
 
-li = [
-    'sword',
-    'horse',
-    'sigar',
-    'sombrero',
-]
-json_li_string = json.dumps(li)
-print('json_li_string ->', type(json_li_string), json_li_string)
-new_li_object = json.loads(json_li_string)
-print('new_li_object ->', type(new_li_object), new_li_object)
+# CSV
+import csv
 
-with open('files/serialize.txt', 'wt') as file:
-    json.dump(user, file)
+# with open('files/Книга1.csv', 'rt') as file:
+#     reader = csv.reader(file, delimiter=';')
+#     headers = next(reader)  # получение заголовков столбцов
+#     print(headers)
+#     print(list(reader))
+#     # for i in reader:
+#     #     print(i, end=' - ')
 
-with open('files/serialize.txt', 'rt') as file:
-    new_object = json.load(file)
-    print('new_object ->', type(new_object), new_object)
+with open('files/Книга1.csv', 'rt') as file:
+    reader = csv.DictReader(file, delimiter=';')
+    for row in reader:
+        print(row)
+
+with open('files/serialize.csv', 'wt') as file:
+    writer = csv.DictWriter(file, fieldnames=user.keys(), delimiter=';')
+    writer.writeheader()
+    writer.writerow(user)
